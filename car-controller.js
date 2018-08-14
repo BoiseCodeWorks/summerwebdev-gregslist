3822f8f696914ee19b9fa2d89a498ac772ae2c4b
Jake Overall <joverall22@gmail.com>
2018-08-13 20:06:34 -0600

    delete and put

--- car-controller.js
+++ car-controller.js
@@ -6,17 +6,17 @@ function CarController() {
     let template = `
     <form onsubmit="app.controllers.carController.makeCar(event)">
       <label for="make">Make</label>
-      <input type="text" name="make">
+      <input type="text" name="make" required>
       <label for="model">Model</label>
-      <input type="text" name="model">
+      <input type="text" name="model" required>
       <label for="imgUrl">Image Url</label>
-      <input type="text" name="imgUrl">
+      <input type="text" name="imgUrl" required>
       <label for="year">Year</label>
-      <input type="number" name="year">
+      <input type="number" name="year" required>
       <label for="price">Price</label>
-      <input type="number" name="price">
+      <input type="number" name="price" required>
       <label for="description">Description</label>
-      <input type="text" name="description">
+      <input type="text" name="description" required>
       <button type="submit">Make Car</button>
     </form>
     <div id="cars" class="row"></div>
@@ -24,6 +24,39 @@ function CarController() {
     document.getElementById('maker').innerHTML = template
     carService.loadCars(draw)
   }
+
+  this.drawEditForm = function (id) {
+    let car = carService.getCar(id)
+    let template = `
+    <form onsubmit="app.controllers.carController.editCar(event)">
+      <label for="make">Make</label>
+      <input type="text" name="make" value="${car.make}" required>
+      <label for="model">Model</label>
+      <input type="text" name="model" value="${car.model}" required>
+      <label for="imgUrl">Image Url</label>
+      <input type="text" name="imgUrl" value="${car.imgUrl}" required>
+      <label for="year">Year</label>
+      <input type="number" name="year" value="${car.year}" required>
+      <label for="price">Price</label>
+      <input type="number" name="price" value="${car.price}" required>
+      <label for="description">Description</label>
+      <input type="text" name="description" value="${car.description}" required>
+      <input type="text" name="id" value="${car._id}" hidden>
+      <button type="submit">Edit Car</button>
+    </form>
+    `
+    document.getElementById('edit').innerHTML = template
+
+  }
+
+  this.editCar = function (event) {
+    event.preventDefault()
+    let formData = event.target
+    carService.editCar(formData, draw)
+    formData.reset()
+    document.getElementById('edit').innerHTML = ''
+  }
+
 
   function draw() {
     let cars = carService.getCars()
@@ -39,12 +72,19 @@ function CarController() {
         <p>${car.price}</p>
         <p>${car.description}</p>
         <img src="${car.imgUrl}" alt="">
-      </div>
+        <button onclick="app.controllers.carController.drawEditForm('${car._id}')"> Edit </button>
+        <button onclick="app.controllers.carController.deleteCar('${car._id}')">Delete</button>
+      
+        </div>
       `
     }
     document.getElementById('cars').innerHTML = template
   }
 
+
+  this.deleteCar = function (id) {
+    carService.deleteCar(id, draw)
+  }
 
   this.makeCar = function (event) {
     event.preventDefault()
--- car-service.js
+++ car-service.js
@@ -5,7 +5,7 @@ function CarService() {
   let cars = []
   cars.push(new Car("Ford", "Aspire", "http://placehold.it/200x200", 1970, 200, "It is NOT aspiring to be anything"))
 
-  function Car(make, model, imgUrl, year, price, description) {
+  function Car(make, model, imgUrl, year, price, description, id) {
 
     this.make = make
 
@@ -19,13 +19,15 @@ function CarService() {
 
     this.description = description
 
+    this._id = id
+
   }
 
   this.getCars = function () {
     let carsCopy = []
     for (let i = 0; i < cars.length; i++) {
       const car = cars[i];
-      let carCopy = new Car(car.make, car.model, car.imgUrl, car.year, car.price, car.description)
+      let carCopy = new Car(car.make, car.model, car.imgUrl, car.year, car.price, car.description, car._id)
       carsCopy.push(carCopy)
     }
     return carsCopy
@@ -34,9 +36,56 @@ function CarService() {
   this.loadCars = function (draw) {
     $.get('https://bcw-gregslist.herokuapp.com/api/cars').then(res => {
       cars = res.data
+      console.log(cars)
       draw()
     })
   }
+
+  this.getCar = function (id) {
+    let car = {}
+    cars.forEach(currentCar => {
+      if (currentCar._id == id) {
+        car = currentCar
+      }
+    })
+    return car
+
+  }
+
+  this.editCar = function (formData, draw) {
+    let editedCar = new Car(
+      formData.make.value,
+      formData.model.value,
+      formData.imgUrl.value,
+      formData.year.value,
+      formData.price.value,
+      formData.description.value,
+      formData.id.value
+    )
+    $.ajax({
+      url: 'https://bcw-gregslist.herokuapp.com/api/cars/' + editedCar._id,
+      method: 'PUT',
+      data: editedCar
+    }).then(res => {
+      this.loadCars(draw)
+      console.log(res)
+    })
+
+
+  }
+
+  this.deleteCar = function (id, draw) {
+    $.ajax({
+      url: 'https://bcw-gregslist.herokuapp.com/api/cars/' + id,
+      method: 'DELETE'
+    }).then(res => {
+      console.log(res)
+      this.loadCars(draw)
+    }).catch(err => {
+      console.error(err.responseJSON.message)
+    })
+  }
+
 
   this.makeCar = function (data, draw) {
     let newCar = new Car(
@@ -47,9 +96,14 @@ function CarService() {
       data.price.value,
       data.description.value
     )
+    delete newCar._id
+    console.log(newCar)
     $.post('https://bcw-gregslist.herokuapp.com/api/cars', newCar).then(res => {
       console.log(res)
+      cars.push(newCar)
       carService.loadCars(draw)
+    }).catch(err => {
+      console.error(err.responseJSON.message)
     })
 
   }
